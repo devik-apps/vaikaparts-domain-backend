afd73f3207d5ec41f1b37c9b3003a3ffad4c4fbd
Abega1642 <hei.abega.2@gmail.com>
2026-02-15 15:10:56 +0300

    refactor(mapper): fix null mapping of user models

--- src/main/java/com/devikapps/vaikaparts/mapper/user/SellerMapper.java
+++ src/main/java/com/devikapps/vaikaparts/mapper/user/SellerMapper.java
@@ -3,11 +3,11 @@ package com.devikapps.vaikaparts.mapper.user;
 import com.devikapps.vaikaparts.model.user.Seller;
 import com.devikapps.vaikaparts.repository.model.user.JSeller;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = ValueObjectMapper.class)
 public interface SellerMapper {
 
-  Seller toDomain(JSeller jSeller);
-
+  @Mapping(target = "profileImgKey", ignore = true)
   JSeller toPersistence(Seller seller);
 }
